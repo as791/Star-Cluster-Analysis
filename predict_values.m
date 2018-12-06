c36019a7c85f5cbb9d05c6312d9c012f4647b9f3
Aryaman Sinha <40312186+as791@users.noreply.github.com>
2018-12-06 12:40:01 +0530

    Upgarded_predict_values

--- predict_values.m
+++ predict_values.m
@@ -3,6 +3,18 @@ function final_Y=predict_values(X,Y)
     Y(Y==9999.90)=NaN;
     X_train=[];Y_train=[];
     X_test=[];
+    if size(X,1)>size(Y,1)      
+        for i=1:size(Y,1)
+            if ~isnan(Y(i))
+                X_train=[X_train;X(i,:)];
+                Y_train=[Y_train;Y(i)];
+            else
+                 X_test=[X_test;X(i,:)];
+            end
+        end
+        X_test=[X_test;X(i+1:end,:)];
+    
+    else    
     for i=1:size(X,1)
         if ~isnan(Y(i))
             X_train=[X_train;X(i,:)];
@@ -11,6 +23,7 @@ function final_Y=predict_values(X,Y)
              X_test=[X_test;X(i,:)];
         end
 
+    end
     end
     [Z_train,~]=pca(X_train,1);
     [Z_test,~]=pca(X_test,1);
